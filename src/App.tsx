ff1086af39ef80d6665f4bd92be32f61a3baa9e8
zghost10 <rafael.karoso@gmail.com>
2023-02-26 14:39:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div
+      className="flex justify-center items-center h-full bg-gray-900 text-white"
+    >
+      <h1 className="font-mono">Project Start!</h1>
+    </div>
+  )
+}
